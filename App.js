02f6307a9c1318c087a17cc0c4a93de99af02f28
penelopa2002 <81851053+penelopa2002@users.noreply.github.com>
2021-04-15 19:50:25 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavBar } from './src/NavBar';
+import { AddTodo } from './src/AddTodo';
+import { Todo } from './src/Todo';
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (title) => {
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title: title,
+      },
+    ]);
+  };
+
+  return (
+    <View>
+      <NavBar title="Todo App" />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+        <View>
+          {todos.map((todo) => (
+            <Todo key={todo.id} todo={todo} />
+          ))}
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+  },
+});
